d19ab5dce94269841f5349757e72d0211439bd72
elidje aka emmanuel <bl4gg7000@gmail.com>
2018-12-09 19:13:24 +0000

    change environnemet to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://restcountries.eu/rest/v2',
+  apiKey: 'AIzaSyAqHR9KE3zTg621_Ee0DHfRzov18cVIFfc',
+  firebase: {
+    apiKey: "AIzaSyBoGn-SvRzniT6htOg8eW6EMfx9sy3g7kI",
+    authDomain: "operantictestcountries.firebaseapp.com",
+    databaseURL: "https://operantictestcountries.firebaseio.com",
+    projectId: "operantictestcountries",
+    storageBucket: "operantictestcountries.appspot.com",
+    messagingSenderId: "415457338661"
+  }
 };
